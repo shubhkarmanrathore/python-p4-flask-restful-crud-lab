fe95595578d35419c8519254a90d1913b955b48f
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-09-17 08:23:03 -0700

    adds lab

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,7 +18,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -27,9 +26,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,35 +37,37 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-    
+
     def patch(self, id):
         plant = Plant.query.filter_by(id=id).first()
-        edit_plant = request.get_json()
-        for attr in edit_plant:
-            setattr(plant, attr, edit_plant[attr])
-        
-        dict = plant.to_dict()
+        patch_plant = request.get_json()
+        for attr in patch_plant:
+            setattr(plant, attr, patch_plant[attr])
 
-        return make_response(jsonify(dict), 200)
+        plant_dict = plant.to_dict()
+        response = make_response(jsonify(plant_dict), 200)
+        return response
 
     def delete(self, id):
-        plant = Plant.query.filter(Plant.id==id).first()
+        plant = Plant.query.filter_by(id=id).first()
         db.session.delete(plant)
-        db.session.commit
+        db.session.commit()
 
-        return make_response(jsonify(f"Plant has been deleted"), 204)
-    
+        response_body = f""
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        response = make_response(jsonify(response_body), 204)
+        return response
 
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
